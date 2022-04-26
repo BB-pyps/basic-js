5a03ba8e86b5b94a79910f9a0465061979a5d36c
BB-pyps <80970001+BB-pyps@users.noreply.github.com>
2022-04-27 01:39:53 +0300

    feat: finish hanoi-tower task

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if(date == null){
+    return `Unable to determine the time of year!`;
+  } else if(!isNaN(date.getTime())){
+   return new Error ('Invalid date!');
+  } else {
+    return `Correct`;
+  }
 }
+  // let myDate = new Date(date);
+  // if (myDate.getFullYear() && myDate.getMonth() && myDate.getDate()) {
+  //   console.log('корректна');
+  // } else {
+  //   console.log('некорректна');
+  // }
+  // if(Object.prototype.toString.call(date) === '[object Date]'){
+  // let myDate = new Date(date);
+  // let month = myDate.getMonth(); 
+  // console.log(month);
+  // return month;
+  // } else {
+  //   return `Invalid date!`;
+  // }
+  // if(month == 'Invalid Date'){
+  //   return `Invalid date!`
+  // }
 
 module.exports = {
   getSeason
