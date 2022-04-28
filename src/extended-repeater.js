e454ea5f1fd51bcca041a2f7a467c08e850fe974
BB-pyps <80970001+BB-pyps@users.noreply.github.com>
2022-04-28 13:28:00 +0300

    feat: finish extended-repeat task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater (str, options) {
+  if(options.separator === undefined){
+    options.separator = `+`;
+  }
+  if(options.additionSeparator === undefined){
+    options.additionSeparator = `|`;
+  }
+  function repeatAddition(){
+    if(options.addition !== undefined){
+    let result = [String(str)];
+    console.log(result);
+    if(options.additionRepeatTimes === undefined){
+      result.push(`${String(options.addition)}`);
+    } else {
+      let i = 0;
+      while(i < options.additionRepeatTimes){
+        if(i === options.additionRepeatTimes - 1){
+          result.push(`${String(options.addition)}`);
+        } else {
+          result.push(`${String(options.addition)}${options.additionSeparator}`);
+        }
+        i++;
+      }
+    }
+    str = result.join('');
+    }
+  return str;
+}
+
+if(options.repeatTimes !== undefined){
+  let i = 0;
+  let endResult = [];
+  str = repeatAddition();
+  while(i < options.repeatTimes){
+    if(i === options.repeatTimes - 1){
+      endResult.push(`${str}`);
+    } else {
+      endResult.push(`${str}${options.separator}`);
+    }
+    i++;
+  }
+  str = endResult.join('');
+  } else {
+    str = repeatAddition();
+  }
+  return str;
 }
 
 module.exports = {
