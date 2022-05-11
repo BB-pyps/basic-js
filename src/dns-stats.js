3a7ef29eb304f6c8c40cbe56073da39902bbfdee
BB-pyps <80970001+BB-pyps@users.noreply.github.com>
2022-05-11 23:09:09 +0300

    feat: finish dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let arr = domains;
+  for(let i = 0; i < arr.length; i++){
+    arr[i] = arr[i].split('.').reverse();
+  }
+  let obj = {};
+  for(let i = 0; i < arr.length; i++){
+    let str = ``;
+    for(let j = 0; j < arr[i].length; j++){
+      str += `.${arr[i][j]}`;
+      if(obj.hasOwnProperty(str)){
+        obj[str] += 1;
+      } else {
+        obj[str] = 1;
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = {
