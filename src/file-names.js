8c8bc5ee6f8852bed2cc602c4abcf494682427bc
BB-pyps <80970001+BB-pyps@users.noreply.github.com>
2023-04-14 14:08:36 +0300

    feat: carbon-dating - extended-repeat tasks

--- src/file-names.js
+++ src/file-names.js
@@ -15,11 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let arr = [];
+  for(let i=0; i < names.length; i++){
+    if(!names.slice(0, i).includes(names[i])){
+      console.log(names[i]);
+      arr.push(names[i]);
+    }
+  }
+  console.log(arr);
+  return arr;
 }
 
 module.exports = {
   renameFiles
 };
+
+
